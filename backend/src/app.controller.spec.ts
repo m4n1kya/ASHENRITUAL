32c6acbabb8fc79cf52245ccefdecbb06b4bdf96
m4n1kya <m4n1kya2005@gmail.com>
2026-09-05 12:05:07 +0530

    Docs: Add fileoverview architecture header to app.controller.spec.ts

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview ASHENRITUAL Architecture
+ * @module app.controller.spec.ts
+ */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
