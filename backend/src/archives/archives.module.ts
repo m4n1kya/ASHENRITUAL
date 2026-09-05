53c6fb9b4fe9595a287ad7f87f7ab32da98c22ff
m4n1kya <m4n1kya2005@gmail.com>
2026-09-05 12:05:13 +0530

    Docs: Add fileoverview architecture header to archives.module.ts

--- backend/src/archives/archives.module.ts
+++ backend/src/archives/archives.module.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview ASHENRITUAL Architecture
+ * @module archives.module.ts
+ */
 import { Module } from '@nestjs/common';
 import { ArchivesController } from './archives.controller';
 import { ArchivesService } from './archives.service';
