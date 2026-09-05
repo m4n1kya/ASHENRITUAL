c642f3bca56d3f618c1fbe5c96750b2c7cbbe892
m4n1kya <m4n1kya2005@gmail.com>
2026-09-05 12:05:14 +0530

    Docs: Add fileoverview architecture header to reviews.module.ts

--- backend/src/reviews/reviews.module.ts
+++ backend/src/reviews/reviews.module.ts
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview ASHENRITUAL Architecture
+ * @module reviews.module.ts
+ */
 import { Module } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
