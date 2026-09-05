2c6155869cdadabc85ceb28754a78bf5a8f6f3b6
m4n1kya <m4n1kya2005@gmail.com>
2026-09-05 12:04:58 +0530

    Docs: Add fileoverview architecture header to button.spec.tsx

--- frontend/src/components/ui/button.spec.tsx
+++ frontend/src/components/ui/button.spec.tsx
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview ASHENRITUAL Architecture
+ * @module button.spec.tsx
+ */
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
